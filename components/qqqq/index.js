87cbd382a09c57b428aefedfebbad0c3d087aa41
C_Y_C <app030242@gmail.com>
2020-03-01 19:00:08 +0800

    1

--- components/qqqq/index.js
+++ components/qqqq/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 // import { Mode } from '../';
 // import Ic_xicn from '@components/ic_xicn/';
-import './css.sass';
+import './css.scss';
 
 const Module = () => {
     const [value, setvalue] = React.useState('asda');
